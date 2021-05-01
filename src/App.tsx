d88d3f51ea67c8a551cf7bc16619188d802ea6bd
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-05-01 22:56:05 +0900

    update: 例外エラー発生ボタン追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn:
+    "https://0f72329b55ec4df58527870c31c11e91@o577387.ingest.sentry.io/5732089",
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <button
+          type="button"
+          onClick={() => {
+            throw new Error("Test Error");
+          }}
+        >
+          throwError
+        </button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
