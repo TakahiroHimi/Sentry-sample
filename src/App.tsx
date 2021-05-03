bb3c595f43e20c3dfd69bc3882dc6075d73ffc7c
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-05-03 16:13:30 +0900

    update: 不要な設定を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
-  environment: process.env.REACT_APP_SENTRY_ENVIRONMENT,
+  // environment: process.env.REACT_APP_SENTRY_ENVIRONMENT,
   autoSessionTracking: true,
+  // release: "himi_local@" + 0.1,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
 });
 
 function App() {
@@ -14,7 +18,7 @@ function App() {
       <button
         type="button"
         onClick={() => {
-          throw new Error("Test Error");
+          throw new Error("Test Error！");
         }}
       >
         throwError
